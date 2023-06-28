a6e4a2dea327eae359123e68163a3834d782341f
malcomowilla <owillamalcom@gmail.com>
2023-06-28 18:06:03 +0300

    s

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,28 @@ Rails.application.routes.draw do
   resources :directors, only: [:index, :show]
   resources :reviews, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+
+  
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
